476f49575ac7f0114b125260d6db818b7a5ffef8
guillaume-L <guillaume.lebot@free.fr>
2017-10-13 13:32:43 +0200

    modification des commentaires sur le fichier Presentation

--- Presentation.php
+++ Presentation.php
@@ -3,6 +3,8 @@
 class Presentation{
   public static $magicien = 'Je suis un magicien et je jette des boule de feu ';
   public static $guerrier = 'je suis un guerrier et je vous empale avec mon éppée';
+  /*les methodes static appartiennent à la class et non pas à l instancier
+  oubiez donc le $this et dites bonjour au ::self*/
   /*
   *methode static qui affiche le message de Presentation du magicien
   */
